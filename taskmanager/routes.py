d45bf43a0b961f42da037df7d72f5cf0fa2bba76
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2024-12-07 08:50:26 +0000

    add files

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -7,7 +7,6 @@ from taskmanager.models import Category, Task
 def home():
     return render_template("tasks.html")
 
-
 @app.route("/categories")
 def categories():
     categories = list(Category.query.order_by(Category.category_name).all())
@@ -22,3 +21,21 @@ def add_category():
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
+
+
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    return redirect(url_for("categories"))
